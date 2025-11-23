e5a4ae5967519a6d5d040a2bc5ecc5032fde7518
x9code <kumarranad672@gmail.com>
2025-11-23 22:43:56 +0530

    remove duplicate string

---
+++ src/java1/string/DuplicateString.java
@@ -0,0 +1,22 @@
+package java1.string;
+
+public class DuplicateString {
+    public static void main(String[] args) {
+        String str="kumarr";
+        int count=0;
+        char a []=str.toCharArray();
+        for (int i=0;i<a.length;i++){
+            for (int j=i+1;j<a.length;j++){
+                if (a[i]==a[j]){
+                    System.out.println("duplicate character in the string are:"+a[j]);
+                    count++;
+                    System.out.println("repeeat="+count);
+
+
+                }
+
+
+            }
+        }
+    }
+}
