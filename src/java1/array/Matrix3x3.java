646a4c6ed1212480239e75a9080993b30a4d2a29
x9code <kumarranad672@gmail.com>
2025-11-25 00:03:06 +0530

    matrix trtanspose

---
+++ src/java1/array/Matrix3x3.java
@@ -0,0 +1,29 @@
+package java1.array;
+public class Matrix3x3 {
+	public static void main(String[] args) {
+		 int[][] matrix = {
+		            {1, 2, 3},
+		            {4, 5, 6},
+		            {7, 8, 9}
+		        };
+
+		        int[][] transpose = new int[3][3];
+
+		        // Finding transpose
+		        for (int i = 0; i < 3; i++) {
+		            for (int j = 0; j < 3; j++) {
+		                transpose[j][i] = matrix[i][j];
+		            }
+		        }
+
+		        System.out.println("Transpose of the matrix:");
+
+		        for (int i = 0; i < 3; i++) {
+		            for (int j = 0; j < 3; j++) {
+		                System.out.print(transpose[i][j] + " ");
+		            }
+		            System.out.println();
+		        }
+		
+	}
+}
