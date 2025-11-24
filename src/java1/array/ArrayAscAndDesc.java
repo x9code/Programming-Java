01cfbf1bc607e8064a2e3a96017f690920957124
x9code <kumarranad672@gmail.com>
2025-11-24 23:55:22 +0530

    array element asc and desc

---
+++ src/java1/array/ArrayAscAndDesc.java
@@ -0,0 +1,34 @@
+package java1.array;
+
+public class ArrayAscAndDesc {
+	public static void main(String[] args) {
+		int[] arr = {4,5,2,7,8,1,9,6};
+		int n = arr.length;
+		int mid = n/2;
+		//sorting the whole array
+		for(int i = 0 ; i< n;i++) {
+			for(int  j = i+1 ;j<n;j++) {
+				if(arr[i] > arr[j]) {
+					int temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+	}
+		// reversing the right half to make it descending
+
+		int left = mid;
+		int right = n-1;
+		while(left < right) {
+			int temp = arr[left];
+			arr[left] = arr[right];
+			arr[right] = temp;
+			left++;
+			right--;
+		}
+		System.out.println("result is : ");
+		for (int i : arr) {
+			System.out.print(i+",");
+		}
+	}
+}
