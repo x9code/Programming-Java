f32f3db04733c2c07730dbf3ad012e356487a9cf
x9code <kumarranad672@gmail.com>
2025-11-26 15:57:41 +0530

    fibonacci

--- src/java1/numbers/Fibonacci.java
+++ src/java1/numbers/Fibonacci.java
@@ -4,29 +4,18 @@ import java.util.Scanner;
 
 public class Fibonacci {
     public static void main(String[] args) {
-//        //0 1 1 2 3 5 8 13 21 34
-//        int series=5;
-//        int n1=0,  n2=1,  sum=0 ;
-//        System.out.print(n1+" " +n2);
-//        for (int i=1;i<series;i++){
-//            sum=n1+n2;
-//            System.out.print(" "+sum);
-//            n1=n2;
-//            n2=sum;
-//        }
-//
-
-        Scanner sc=new Scanner(System.in);
-        System.out.println("enter a number");
-        int Series=sc.nextInt();
-        int n1=0,  n2=1,  sum=0 ;
-        System.out.print(n1+" " +n2);
-        for (int i=2;i<Series;i++) {
+        System.out.println("Enter a number : ");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int n1 = 0;
+        int n2 = 1;
+        int sum = 0;
+        System.out.print(n1+" "+n2);
+        for (int i = 2 ; i <= num ; i++){
             sum = n1 + n2;
-            System.out.print(" " + sum);
+            System.out.print(" "+sum);
             n1 = n2;
             n2 = sum;
         }
-
     }
 }
