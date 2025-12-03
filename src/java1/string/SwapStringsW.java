af27167bc71af49e2a5c494061217b511cbfb4b0
Deepak <contact.deepakrana.work@gmail.com>
2025-12-03 19:58:56 +0530

    swap two strings without using temp string with scanner

--- src/java1/string/SwapStringsW.java
+++ src/java1/string/SwapStringsW.java
@@ -1,9 +1,15 @@
 package java1.string;
 
+import java.util.Scanner;
+
 public class SwapStringsW {
     public static void main(String[] args) {
-        String str1 = "Hello";
-        String str2 = "World";
+        System.out.println("Enter string 1 :");
+        Scanner sc = new Scanner(System.in);
+        String str1 = sc.nextLine();
+        System.out.println("Enter string 1 :");
+        Scanner sc1 = new Scanner(System.in);
+        String str2 = sc1.nextLine();
         System.out.println("Before swapping : ");
         System.out.println("str1 :" + str1);
         System.out.println("str2 :"+ str2);
