2b9c6aa9dc004ada92bf38e502328a5fda443ddf
Deepak <contact.deepakrana.work@gmail.com>
2025-12-03 20:26:57 +0530

    string lowercase to uppercase

--- src/java1/string/LowerToUpperCase.java
+++ src/java1/string/LowerToUpperCase.java
@@ -3,7 +3,7 @@ package java1.string;
 import java.util.Scanner;
 import java.util.SortedMap;
 
-public class LowerCaseToUpperCase {
+public class LowerToUpperCase {
     public static void main(String[] args) {
         System.out.println("Enter a string :");
         Scanner sc = new Scanner(System.in);
