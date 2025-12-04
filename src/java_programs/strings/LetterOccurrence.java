fe72bb5f1a3ddc27ef20798970629b1aa2fe8de6
Deepak <contact.deepakrana.work@gmail.com>
2025-12-04 20:18:22 +0530

    letter count in a string

---
+++ src/java_programs/strings/LetterOccurrence.java
@@ -0,0 +1,23 @@
+package java_programs.strings;
+
+import java.util.LinkedHashMap;
+
+public class LetterOccurrence {
+    public static void letterCount(String s){
+        LinkedHashMap<Character,Integer> map = new LinkedHashMap<>();
+        String str = s.replaceAll("\\s","");
+        char[] ch = str.toCharArray();
+        for (char c : ch){
+            if (map.containsKey(c)){
+                map.put(c,map.get(c)+1);
+            }else {
+                map.put(c,1);
+            }
+        }
+        System.out.println(map);
+    }
+    public static void main(String[] args) {
+        String s = "hello my name is deepak kumar rana";
+        letterCount(s);
+    }
+}
