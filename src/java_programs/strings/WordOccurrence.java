74a345d419b14e3a59b6ae29f280ff1e73014e40
Deepak <contact.deepakrana.work@gmail.com>
2025-12-04 20:07:59 +0530

    word occurance in a string

---
+++ src/java_programs/strings/WordOccurrence.java
@@ -0,0 +1,22 @@
+package java_programs.strings;
+
+import java.util.LinkedHashMap;
+
+public class WordOccurrence {
+    public static LinkedHashMap<String ,Integer> count(String str){
+        LinkedHashMap<String, Integer> map = new LinkedHashMap<>();
+        String[] st = str.split(" ");
+        for(String w : st){
+            if (map.containsKey(w)){
+                map.put(w,map.get(w)+1);
+            }else {
+                map.put(w,1);
+            }
+        }
+        return  map;
+    }
+    public static void main(String[] args) {
+        String s = "Java is a programming language and java is old language";
+        System.out.println(count(s.toLowerCase()));
+    }
+}
