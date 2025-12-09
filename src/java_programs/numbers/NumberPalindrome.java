ec87572b89f5c37e6aa5403b7404e5734291f2be
Deepak <contact.deepakrana.work@gmail.com>
2025-12-09 19:26:20 +0530

    number palindrome

---
+++ src/java_programs/numbers/NumberPalindrome.java
@@ -0,0 +1,19 @@
+package java_programs.numbers;
+
+public class NumberPalindrome {
+    public static void main(String[] args) {
+        int num = 12121;
+        int originalNum = num;
+        int rev = 0;
+        while (num != 0) {
+        int digit = num % 10;
+        rev = rev * 10 + digit;
+        num = num / 10;
+        }
+        if (originalNum == rev) {
+            System.out.println("Palindrome");
+        }else{
+            System.out.println("Not palindrome");
+        }
+   }
+}
